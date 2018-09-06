fd19740e4af3eefa09497462974ef6ec88215f6b
tang002GU <tang002@gannon.edu>
2018-09-06 04:06:06 +0000

    Changed JavaHelloWorld.java to print Hello with numbber and HTMLHello.html to have a body p

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -4,5 +4,9 @@
 public class JavaHelloWorld {
 	public static void main(String[] args) {
     	System.out.println("Java Hello from AWS Cloud9!");
+    	
+    	for(int i=0; i<10; i++){
+    		System.out.println("Hello "+i);
+    	}
 	}
 }
